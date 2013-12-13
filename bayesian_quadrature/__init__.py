540454b8f23803694d30942ef1e0b422b982c34f
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-12-12 21:00:25 -0800

    Add logging to __init__

--- bayesian_quadrature/__init__.py
+++ bayesian_quadrature/__init__.py
@@ -1,2 +1,9 @@
+import logging
+FORMAT = '%(levelname)s -- %(processName)s/%(filename)s -- %(message)s'
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger("bayesian_quadrature")
+logger.setLevel("INFO")
+
+
 from bq import BQ
 __all__ = ['BQ']
