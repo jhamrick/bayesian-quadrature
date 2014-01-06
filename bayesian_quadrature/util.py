4234cc5756ca0103415eafd1bb7c62fa7edd6f87
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-01-06 00:00:34 -0800

    Move some helper methods into util

--- bayesian_quadrature/util.py
+++ bayesian_quadrature/util.py
@@ -1,7 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import logging
 
-from util_c import slice_sample as _slice_sample
+from . import bq_c
+from .util_c import slice_sample as _slice_sample
+
+logger = logging.getLogger("bayesian_quadrature.util")
+DTYPE = np.dtype('float64')
+PREC = np.finfo(DTYPE).precision / 2.0
 
 
 def set_scientific(ax, low, high, axis=None):
@@ -63,3 +69,74 @@ def slice_sample(logpdf, niter, w, xval, nburn=1, freq=1):
     # don't return burnin samples or inbetween samples
     out = samples[nburn:][::freq]
     return out
+
+
+def make_gp_loglh(gp, params):
+    def loglh(x):
+        for p, v in zip(params, x):
+            try:
+                gp.set_param(p, v)
+            except ValueError:
+                return -np.inf
+        llh = gp.log_lh
+        return llh
+    return loglh
+
+
+def vlines(ax, x, **kwargs):
+    ymin, ymax = ax.get_ylim()
+    ax.vlines(x, ymin, ymax, **kwargs)
+    ax.set_ylim(ymin, ymax)
+
+
+def hlines(ax, y, **kwargs):
+    xmin, xmax = ax.get_xlim()
+    ax.hlines(y, xmin, xmax, **kwargs)
+    ax.set_xlim(xmin, xmax)
+
+
+def improve_conditioning(gp):
+    Kxx = gp.Kxx
+    cond = np.linalg.cond(Kxx)
+    logger.debug("Kxx conditioning number is %s", cond)
+
+    if hasattr(gp, "jitter"):
+        jitter = gp.jitter
+    else:
+        jitter = np.zeros(Kxx.shape[0], dtype=DTYPE)
+        gp.jitter = jitter
+
+    # the conditioning is really bad -- just increase the variance
+    # a little for all the elements until it's less bad
+    idx = np.arange(Kxx.shape[0])
+    while np.log10(cond) > PREC:
+        logger.debug("Adding jitter to all elements")
+        bq_c.improve_covariance_conditioning(Kxx, jitter, idx=idx)
+        cond = np.linalg.cond(Kxx)
+        logger.debug("Kxx conditioning number is now %s", cond)
+
+    # now improve just for those elements which result in a
+    # negative variance, until there are no more negative elements
+    # in the diagonal
+    gp._memoized = {'Kxx': Kxx}
+    var = np.diag(gp.cov(gp._x))
+    while (var < 0).any():
+        idx = np.nonzero(var < 0)[0]
+
+        logger.debug("Adding jitter to indices %s", idx)
+        bq_c.improve_covariance_conditioning(Kxx, jitter, idx=idx)
+
+        Kxx = gp.Kxx
+        gp._memoized = {'Kxx': Kxx}
+        var = np.diag(gp.cov(gp._x))
+        cond = np.linalg.cond(Kxx)
+        logger.debug("Kxx conditioning number is now %s", cond)
+
+
+def improve_tail_covariance(gp):
+    Kxx = gp.Kxx
+    gp._memoized = {'Kxx': Kxx}
+    max_jitter = np.diag(Kxx).max() * 1e-2
+    jitter = np.clip(-gp.x * 1e-4, 0, max_jitter)
+    Kxx += np.eye(gp.x.size) * jitter
+    gp.jitter = jitter
