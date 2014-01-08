8e79a8ebbea86a178ae8a8543ea08019200a903c
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-01-08 14:25:25 -0800

    Update docstrings for bq_c.pyx

--- bayesian_quadrature/bq_c.pyx
+++ bayesian_quadrature/bq_c.pyx
@@ -31,12 +31,52 @@ cdef float64_t NAN = np.nan
 ######################################################################
 
 cpdef float64_t vonmises_logpdf(float64_t x, float64_t mu, float64_t kappa):
+    r"""
+    Computes the log-PDF for the Von Mises distribution at location
+    :math:`x`:
+
+    .. math ::
+    
+        p(x | \mu, \kappa) = \frac{\exp(\kappa\cos(x-\mu))}{2\pi I_0(\kappa)}
+
+    Where :math:`I_0` is the Bessel function of order 0.
+
+    Parameters
+    ----------
+    x : float64_t
+        Input angle (radians)
+    mu : float64_t
+        Mean angle (radians)
+    kappa : float64_t
+        Spread parameter
+
+    Returns
+    -------
+    out : value of the log-PDF
+
+    """
     C = -log(2 * M_PI * j0(kappa))
     p = C + (kappa * cos(x - mu))
     return p
 
 
 def p_x_gaussian(float64_t[::1] p_x, float64_t[::1, :] x, float64_t[::1] mu, float64_t[::1, :] cov):
+    r"""
+    Computes the Gaussian PDF of input locations :math:`x`.
+
+    Parameters
+    ----------
+    p_x : float64_t[::1]
+        :math:`n` output vector of probabilities
+    x : float64_t[::1, :]
+        :math:`d\times n` input locations
+    mu : float64_t[::1]
+        :math:`d` mean vector
+    cov : float64_t[::1, :]
+        :math:`d\times d` covariance matrix
+
+    """
+
     cdef int d = x.shape[0]
     cdef int n = x.shape[1]
 
@@ -58,6 +98,22 @@ def p_x_gaussian(float64_t[::1] p_x, float64_t[::1, :] x, float64_t[::1] mu, flo
 
 
 def p_x_vonmises(float64_t[::1] p_x, float64_t[::1] x, float64_t mu, float64_t kappa):
+    r"""
+    Computes the Von Mises PDF of input locations :math:`x`.
+
+    Parameters
+    ----------
+    p_x : float64_t[::1]
+        :math:`n` output vector of probabilities
+    x : float64_t[::1, :]
+        :math:`n` input vector of locations
+    mu : float64_t[::1]
+        mean parameter
+    kappa : float64_t[::1, :]
+        spread parameter
+
+    """
+
     cdef int n = x.shape[0]
     cdef int i
     
@@ -71,6 +127,12 @@ def p_x_vonmises(float64_t[::1] p_x, float64_t[::1] x, float64_t mu, float64_t k
 @cython.boundscheck(True)
 @cython.wraparound(True)
 def improve_covariance_conditioning(float64_t[:, ::1] M, float64_t[::1] jitters, long[::1] idx):
+    r"""
+    Add noise to the indices `idx` along the diagonal of `M`. Update
+    the corresponding locations in the `jitters` vector to include
+    this new noise.
+
+    """
     cdef float64_t sqd_jitter = fmax(EPS, np.max(M)) * 1e-4
     cdef int i
     for i in xrange(len(idx)):
@@ -81,6 +143,11 @@ def improve_covariance_conditioning(float64_t[:, ::1] M, float64_t[::1] jitters,
 @cython.boundscheck(True)
 @cython.wraparound(True)
 def remove_jitter(float64_t[:, ::1] M, float64_t[::1] jitters, long[::1] idx):
+    r"""
+    Remove noise from the indices `idx` along the diagonal of `M`. Set
+    the corresponding locations in the `jitters` vector to zero.
+
+    """
     cdef int i
     for i in xrange(len(idx)):
         M[idx[i], idx[i]] -= jitters[idx[i]]
@@ -88,6 +155,39 @@ def remove_jitter(float64_t[:, ::1] M, float64_t[::1] jitters, long[::1] idx):
 
 
 def Z_mean(float64_t[::1, :] x_sc, float64_t[::1] alpha_l, float64_t h_l, float64_t[::1] w_l, float64_t[::1] mu, float64_t[::1, :] cov):
+    r"""
+    Compute the mean of the integral:
+
+    .. math ::
+
+        Z = \int \ell(x)\mathcal{N}(x \big\vert \mu, \Sigma)\ \mathrm{d}x
+
+    where the mean is defined as:
+
+    .. math ::
+    
+        m(Z) = E[Z \big\vert \bar{\ell}(x_sc)]
+
+    Parameters
+    ----------
+    x_sc : float64_t[::1, :]
+        :math:`d\times n` vector of observed and candidate locations
+    alpha_l : float64_t[::1]
+        :math:`K_\ell(x_{sc}, x_{sc})^{-1}\bar{\ell}(x_{sc})`
+    h_l : float64_t
+        output scale parameter for kernel :math:`K_\ell`
+    w_l : float64_t[::1]
+        :math:`d` vector of lengthscales for kernel :math:`K_\ell`
+    mu : float64_t[::1]
+        :math:`d` prior mean
+    cov : float64_t[::1, :]
+        :math:`d\times d` prior covariance
+
+    Returns
+    -------
+    out : mean of :math:`Z`
+
+    """
 
     cdef int nc = x_sc.shape[1]
     cdef int d = x_sc.shape[0]
@@ -114,6 +214,28 @@ def Z_mean(float64_t[::1, :] x_sc, float64_t[::1] alpha_l, float64_t h_l, float6
 
 
 def approx_Z_mean(float64_t[::1, :] xo, float64_t[::1] p_xo, float64_t[::1] l):
+    r"""
+    Approximate the mean of the integral:
+
+    .. math ::
+
+        Z = \int \ell(x)p(x)\ \mathrm{d}x
+
+    Parameters
+    ----------
+    xo : float64_t[::1, :]
+        :math:`d\times n` vector of approximation locations
+    p_xo : float64_t[::1]
+        :math:`n` vector prior probabilities at approximation locations
+    l : float64_t[::1]
+        :math:`n` vector of likelihood evaluated at approximation locations
+
+    Returns
+    -------
+    out : approximate mean of :math:`Z`
+
+    """
+
     cdef int d = xo.shape[0]
     cdef int n = xo.shape[1]
 
@@ -140,6 +262,47 @@ def approx_Z_mean(float64_t[::1, :] xo, float64_t[::1] p_xo, float64_t[::1] l):
 
 
 def Z_var(float64_t[::1, :] x_s, float64_t[::1, :] x_sc, float64_t[::1] alpha_l, float64_t[::1, :] L_tl, float64_t h_l, float64_t[::1] w_l, float64_t h_tl, float64_t[::1] w_tl, float64_t[::1] mu, float64_t[::1, :] cov):
+    r"""
+    Compute the variance of the integral:
+
+    .. math ::
+
+        Z = \int \ell(x)\mathcal{N}(x \big\vert \mu, \Sigma)\ \mathrm{d}x
+
+    where the variance is defined as:
+
+    .. math ::
+    
+        V(Z) = \int\int m_\ell(x) C_{\log\ell}(x, x^\prime) m_\ell(x^\prime) p(x)p(x^\prime)\ \mathrm{d}x\ \mathrm{d}x^\prime
+
+    Parameters
+    ----------
+    x_s : float64_t[::1, :]
+        :math:`d\times n_s` vector of observed locations
+    x_sc : float64_t[::1, :]
+        :math:`d\times n_{sc}` vector of observed and candidate locations
+    alpha_l : float64_t[::1]
+        :math:`K_\ell(x_{sc}, x_{sc})^{-1}\bar{\ell}(x_{sc})`
+    L_tl : float64_t[::1, :]
+        lower-triangular Cholesky factor of :math:`K_{\log\ell}(x_{s}, x_{s})`
+    h_l : float64_t
+        output scale parameter for kernel :math:`K_\ell`
+    w_l : float64_t[::1]
+        :math:`d` vector of lengthscales for kernel :math:`K_\ell`
+    h_tl : float64_t
+        output scale parameter for kernel :math:`K_{\log\ell}`
+    w_tl : float64_t[::1]
+        :math:`d` vector of lengthscales for kernel :math:`K_{\log\ell}`
+    mu : float64_t[::1]
+        :math:`d` prior mean
+    cov : float64_t[::1, :]
+        :math:`d\times d` prior covariance
+
+    Returns
+    -------
+    out : variance of :math:`Z`
+
+    """
 
     cdef int ns = x_s.shape[1]
     cdef int nc = x_sc.shape[1]
@@ -193,6 +356,36 @@ def Z_var(float64_t[::1, :] x_s, float64_t[::1, :] x_sc, float64_t[::1] alpha_l,
 
 
 def approx_Z_var(float64_t[::1, :] xo, float64_t[::1] p_xo, float64_t[::1] m_l, float64_t[::1, :] C_tl):
+    r"""
+    Approximate the variance of the integral:
+
+    .. math ::
+
+        Z = \int \ell(x)p(x)\ \mathrm{d}x
+
+    where the variance is defined as:
+
+    .. math ::
+    
+        V(Z) = \int\int m_\ell(x) C_{\log\ell}(x, x^\prime) m_\ell(x^\prime) p(x)p(x^\prime)\ \mathrm{d}x\ \mathrm{d}x^\prime
+
+    Parameters
+    ----------
+    xo : float64_t[::1, :]
+        :math:`d\times n` vector of approximation locations
+    p_xo : float64_t[::1]
+        :math:`n` vector prior probabilities at approximation locations
+    m_l : float64_t[::1]
+        :math:`n` vector of likelihoods evaluated at approximation locations
+    C_tl : float64_t[::1, :]
+        :math:`n\times n` covariance matrix for :math:`\log\ell` evaluated at approximation locations
+
+    Returns
+    -------
+    out : approximate variance of :math:`Z`
+
+    """
+
     cdef int d = xo.shape[0]
     cdef int n = xo.shape[1]
 
@@ -230,6 +423,33 @@ def approx_Z_var(float64_t[::1, :] xo, float64_t[::1] p_xo, float64_t[::1] m_l, 
 
 
 cdef float64_t _esm(float64_t[::1] int_K_l, float64_t[::1] l_sc, float64_t[::1, :] K_l, float64_t tm_a, float64_t tC_a) except? -1:
+    r"""
+    Computes the expected squared mean of :math:`Z` given a new
+    observation at :math:`x_a`.
+
+    .. math ::
+    
+        E[m(Z)^2 \big\vert x_a] = \int m(Z | \ell_s, \ell_a)^2 \mathcal{N}(\log\ell_a | \hat{m}_a, \hat{C}_a)\ \mathrm{d}\log\ell_a
+
+    Parameters
+    ----------
+    int_K_l : float64_t[::1]
+        :math:`\int K_\ell(x_{sca}, x) p(x)\ \mathrm{d}x`
+    l_sc : float64_t[::1]
+        :math:`n_{sc}` vector of observed and candidate locations
+    K_l : float64_t[::1, :]
+        :math:`n_{sca}\times n_{sca}` kernel matrix :math:`K_{\ell}(x_{sca}, x_{sca})`
+    tm_a : float64_t
+        prior mean of :math:`\log\ell_a`
+    tC_a : float64_t
+        prior variance of :math:`\log\ell_a`
+
+    Returns
+    -------
+    out : expected squared mean of :math:`Z`
+
+    """    
+
     cdef int nca = K_l.shape[0]
     cdef int nc = nca - 1
 
@@ -261,6 +481,41 @@ cdef float64_t _esm(float64_t[::1] int_K_l, float64_t[::1] l_sc, float64_t[::1, 
 
 
 def expected_squared_mean(float64_t[::1] l_sc, float64_t[::1, :] K_l, float64_t tm_a, float64_t tC_a, float64_t[::1, :] x_sca, float64_t h_l, float64_t[::1] w_l, float64_t[::1] mu, float64_t[::1, :] cov):
+    r"""
+    Computes the expected squared mean of :math:`Z` given a new
+    observation at :math:`x_a`.
+
+    .. math ::
+    
+        E[m(Z)^2 \big\vert x_a] = \int m(Z | \ell_s, \ell_a)^2 \mathcal{N}(\log\ell_a | \hat{m}_a, \hat{C}_a)\ \mathrm{d}\log\ell_a
+
+    Parameters
+    ----------
+    l_sc : float64_t[::1]
+        :math:`n_{sc}` vector of observed and candidate locations
+    K_l : float64_t[::1, :]
+        :math:`n_{sca}\times n_{sca}` kernel matrix :math:`K_{\ell}(x_{sca}, x_{sca})`
+    tm_a : float64_t
+        prior mean of :math:`\log\ell_a`
+    tC_a : float64_t
+        prior variance of :math:`\log\ell_a`
+    x_sca : float64_t[::1, :]
+        :math:`d\times n_{sca}` input vector
+    h_l : float64_t
+        output scale kernel parameter for :math:`K_\ell`
+    w_l : float64_t[::1]
+        :math:`d` vector of lengthscales for :math:`K_\ell`
+    mu : float64_t[::1]
+        :math:`d` mean
+    cov : float64_t[::1, :]
+        :math:`d\times d` covariance
+
+    Returns
+    -------
+    out : expected squared mean of :math:`Z`
+
+    """    
+
     cdef int n = x_sca.shape[1]
     cdef float64_t[::1] int_K_l = empty(n, dtype=float64, order='F')
     ga.int_K(int_K_l, x_sca, h_l, w_l, mu, cov)
@@ -269,6 +524,39 @@ def expected_squared_mean(float64_t[::1] l_sc, float64_t[::1, :] K_l, float64_t 
 
 
 def approx_expected_squared_mean(float64_t[::1] l_sc, float64_t[::1, :] K_l, float64_t tm_a, float64_t tC_a, float64_t[::1, :] xo, float64_t[::1] p_xo, float64_t[::1, :] Kxxo):
+    r"""
+    Approximates the expected squared mean of :math:`Z` given a new
+    observation at :math:`x_a`.
+
+    .. math ::
+    
+        E[m(Z)^2 \big\vert x_a] = \int m(Z | \ell_s, \ell_a)^2 \mathcal{N}(\log\ell_a | \hat{m}_a, \hat{C}_a)\ \mathrm{d}\log\ell_a
+
+    Parameters
+    ----------
+    l_sc : float64_t[::1]
+        :math:`n_{sc}` vector of observed and candidate locations
+    K_l : float64_t[::1, :]
+        :math:`n_{sca}\times n_{sca}` kernel matrix :math:`K_{\ell}(x_{sca}, x_{sca})`
+    tm_a : float64_t
+        prior mean of :math:`\log\ell_a`
+    tC_a : float64_t
+        prior variance of :math:`\log\ell_a`
+    xo : float64_t[::1, :]
+        :math:`d\times m` vector of approximation locations
+    Kxxo : float64_t[::1, :]
+        :math:`n_{sca}\times m` kernel matrix
+    mu : float64_t[::1]
+        :math:`d` mean
+    cov : float64_t[::1, :]
+        :math:`d\times d` covariance
+
+    Returns
+    -------
+    out : approximate expected squared mean of :math:`Z`
+
+    """    
+
     cdef int m = Kxxo.shape[0]
     cdef int n = xo.shape[1]
 
@@ -298,6 +586,22 @@ def approx_expected_squared_mean(float64_t[::1] l_sc, float64_t[::1, :] K_l, flo
 
 
 def filter_candidates(float64_t[::1] x_c, float64_t[::1] x_s, float64_t thresh):
+    r"""
+
+    Given a vector of possible candidate locations, :math:`x_c`,
+    filter out locations which are close to one or more observations
+    :math:`x_s`, or to other candidate locations.
+
+    Parameters
+    ----------
+    x_c : float64_t[::1]
+        potential candidate locations
+    x_s : float64_t[::1]
+        observed locations
+    thresh : float64_t
+        minimum allowed distance
+
+    """
     cdef int nc = x_c.shape[0]
     cdef int ns = x_s.shape[0]
     cdef int i, j
@@ -306,6 +610,9 @@ def filter_candidates(float64_t[::1] x_c, float64_t[::1] x_s, float64_t thresh):
 
     while not done:
         done = 1
+
+        # find candidates that are close to each other, and replace
+        # them with their average location
         for i in xrange(nc):
             for j in xrange(i+1, nc):
                 if np.isnan(x_c[i]) or np.isnan(x_c[j]):
@@ -317,6 +624,7 @@ def filter_candidates(float64_t[::1] x_c, float64_t[::1] x_s, float64_t thresh):
                     x_c[j] = NAN
                     done = 1
 
+        # remove candidates that are too close to an observation
         for i in xrange(nc):
             for j in xrange(ns):
                 diff = fabs(x_c[i] - x_s[j])
